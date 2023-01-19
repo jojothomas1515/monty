dc2501b82ab065f613ec6855fb51ff633c75a843
Jojo <jojothomas1515@gmail.com>
2023-01-19 11:24:08 +0100

    task one

--- e_exit.c
+++ e_exit.c
@@ -1,28 +1,44 @@
 #include "monty.h"
 
+/**
+ * f_exit - exit the program with an error message
+ * @n: exit signal
+ * @filename: the file name for detailed error
+ */
 void f_exit(int n, char *filename)
 {
-        switch (n)
-        {
-        case -1:
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-                break;
-        case -2:
-                fprintf(stderr, "Error: Can't open file %s\n", filename);
-                exit(EXIT_FAILURE);
-                break;
-        default:
-                exit(EXIT_FAILURE);
-                break;
-        }
+	switch (n)
+	{
+	case -1:
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+		break;
+	case -2:
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+		break;
+	default:
+		exit(EXIT_FAILURE);
+		break;
+	}
 }
 
+/**
+ * m_exit - exit the program with an error message on malloc related errors
+ */
 void m_exit(void)
 {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
+	fprintf(stderr, "Error: malloc failed\n");
+	exit(EXIT_FAILURE);
 }
+
+/**
+ * op_exit - exit the program with an error message on
+ * opcode/operation related errors
+ * @n: exit signals
+ * @opcode: operation code
+ * @ln: line NUMBER
+ */
 void op_exit(int n, char *opcode, int ln)
 {
 }
