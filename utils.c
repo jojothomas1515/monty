3b4bcd2fd38bfb89362505aab0b783e70d96675a
Jojo <jojothomas1515@gmail.com>
2023-01-21 10:08:20 +0100

    utilities updated

--- utils.c
+++ utils.c
@@ -28,7 +28,7 @@ void parse_and_exec(char *line, int line_num, stack_t **stack)
 		op_exit(-2, tok, line_num, *stack);
 
 	tok2 = strtok(NULL, " ");
-	value = atoi_check(tok2) ? (long)atoi(tok2) : LONG_MIN;
+	value = tok == NULL ? LONG_MIN : (long)atoi(tok2);
 	get_opcode_func(tok)(stack, line_num);
 end_of:
 	(void)NULL;
