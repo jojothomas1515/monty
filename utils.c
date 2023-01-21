fb20c6f8fd399bb37062c458a345638c6888f208
Jojo <jojothomas1515@gmail.com>
2023-01-21 11:17:30 +0100

    why

--- utils.c
+++ utils.c
@@ -28,7 +28,8 @@ void parse_and_exec(char *line, int line_num, stack_t **stack)
 		op_exit(-2, tok, line_num, *stack);
 
 	tok2 = strtok(NULL, " ");
-	value = tok2 ==  NULL ? INT_MIN + 1  : atoi(tok2);
+	
+	value = tok2 ==  NULL ? INT_MIN + 1 : atoi(tok2);
 	get_opcode_func(tok)(stack, line_num);
 end_of:
 	(void)NULL;
